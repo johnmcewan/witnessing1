efc97585e5d4f43336982a5878cd04dfdd6b8093
John McEwan <jmcewan3@yahoo.ca>
2024-10-02 05:17:09 -0500

    updates

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'witnessing.urls'
+ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'witnessing.wsgi.application'
+WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
 # Database
